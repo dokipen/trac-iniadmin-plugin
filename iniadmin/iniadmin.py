11c5442cd50d7fbcd91b5fd75b9b7b76b15a854e
athomas <athomas@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-05-23 06:31:36 +0000

    IniAdminPlugin:
    Ported to 0.11 (hopefully).
    
    git-svn-id: http://trac-hacks.org/svn/iniadminplugin/0.11@2256 7322e99d-02ea-0310-aa39-e9a107903beb

--- iniadmin/iniadmin.py
+++ iniadmin/iniadmin.py
@@ -2,7 +2,7 @@ from trac.core import *
 from trac.wiki.formatter import wiki_to_html
 from trac.web.chrome import ITemplateProvider, add_stylesheet
 from trac.util import escape, Markup, sorted
-from webadmin.web_ui import IAdminPageProvider
+from trac.admin.api import IAdminPanelProvider
 import inspect
 from trac.config import Option, BoolOption, IntOption, ListOption, \
                         ExtensionOption
@@ -12,15 +12,15 @@ except:
     from sets import Set as set
 
 class IniAdminPlugin(Component):
-    implements(ITemplateProvider, IAdminPageProvider)
+    implements(ITemplateProvider, IAdminPanelProvider)
 
     # IAdminPageProvider methods
-    def get_admin_pages(self, req):
+    def get_admin_panels(self, req):
         if req.perm.has_permission('TRAC_ADMIN'):
             for section in sorted(set([s for s, _ in Option.registry])):
                 yield ('tracini', 'trac.ini', section, section)
 
-    def process_admin_request(self, req, cat, page, path_info):
+    def render_admin_panel(self, req, cat, page, path_info):
         assert req.perm.has_permission('TRAC_ADMIN')
         if page not in set([s for s, _ in Option.registry]):
             raise TracError("Invalid section %s" % page)
@@ -40,32 +40,34 @@ class IniAdminPlugin(Component):
                 self.env.config.save()
             req.redirect(self.env.href.admin(cat, page))
 
-            
+
         add_stylesheet(req, 'iniadmin/css/iniadmin.css')
 
         options = sorted([option for (section, _), option in
                           Option.registry.iteritems() if section == page],
                          key=lambda a: a.name)
 
-        hdf_options = []
+        options_data = []
         for option in options:
             doc = wiki_to_html(inspect.getdoc(option), self.env, req)
             value = self.env.config.get(page, option.name)
             # We assume the classes all end in "Option"
             type = option.__class__.__name__.lower()[:-6] or 'text'
-            hdf_option  = {'name': option.name, 'default': option.default,
+            option_data  = {'name': option.name, 'default': option.default,
                            'doc': Markup(doc), 'value': value, 'type': type}
             if type == 'extension':
                 options = []
                 for impl in option.xtnpt.extensions(self):
                     options.append(impl.__class__.__name__)
                 options.sort()
-                hdf_option['options'] = options
-            hdf_options.append(hdf_option)
+                option_data['options'] = options
+            options_data.append(option_data)
 
-        req.hdf['iniadmin.section'] = page
-        req.hdf['iniadmin.options'] = hdf_options
-        return 'iniadmin.cs', None
+        data = {'iniadmin': {
+          'section': page,
+          'options': options_data
+          } }
+        return 'iniadmin.html', data
 
     # ITemplateProvider methods
     def get_templates_dirs(self):
